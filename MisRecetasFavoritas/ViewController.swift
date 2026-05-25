68acc5305ad9234392c196dc9089b271ca8f9865
arzaluz-chris <23008381@es.uveg.edu.mx>
2026-05-25 16:58:26 -0600

    Initial Commit

---
+++ MisRecetasFavoritas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MisRecetasFavoritas
+//
+//  Created by Christian Arzaluz on 25/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
